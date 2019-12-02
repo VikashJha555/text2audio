5d21adcebfc175500363452948df38e5e3ca8e0c
VikashJha555 <35874353+VikashJha555@users.noreply.github.com>
2019-12-03 03:13:16 +0530

    Text to audio(.wav) converter using Google Text to Speech module.

---
+++ text2audio.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+
+import os
+
+f=open('test.txt')
+x=f.read()
+
+language='en'
+
+audio=gTTS(text=x,lang=language,slow=False)
+
+audio.save("test.wav")
+os.system("test.wav")
